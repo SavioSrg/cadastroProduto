163325d010be77999c7da36538d29102fbf4dfaa
Sávio Sérgio <168147001+SavioSrg@users.noreply.github.com>
2025-01-09 14:26:17 -0300

    atualização parte 3

--- js/script-table.js
+++ js/script-table.js
@@ -1,13 +1,17 @@
-window.onload = function() {
+window.onload = function () {
     updateProductList()
-  };
+};
 
-  if (localStorage.hasOwnProperty("products")){
+if (localStorage.hasOwnProperty("products")) {
     products = JSON.parse(localStorage.getItem("products"))
     products.sort((a, b) => a.price - b.price);
-  }else{
+    document.getElementById('list-container').style.display = 'block';
+    document.getElementById('modal-vazio').style.display = 'none';
+
+} else {
+    document.getElementById('remove-products-btn').style.display = 'none';
     alert("Tabela não possui dados, favor inserir dados")
-  }
+}
 
 // Atualizar a tabela de produtos
 function updateProductList() {
@@ -21,6 +25,7 @@ function updateProductList() {
         <td>${product.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td>
         <td>
           <button class="edit-btn" data-index="${index}">Editar</button>
+          <button class="excluir-btn" data-index="${index}">Excluir</button>
         </td>
       `;
         list.appendChild(row);
@@ -34,10 +39,32 @@ function updateProductList() {
             openEditModal(index);
         });
     });
+
+    //Botões de excluir
+    const excluirButtons = document.querySelectorAll('.excluir-btn');
+    excluirButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const confirmDelete = confirm('Tem certeza de que deseja excluir o produto da tabela?');
+            if (confirmDelete) {
+                const index = button.getAttribute('data-index');
+                removeFromLocalStorage(index);
+            }
+        });
+    });
+}
+
+// Função para remover um elemento do localStorage
+function removeFromLocalStorage(index) {
+
+    products.splice(index, 1);
+    localStorage.setItem("products", JSON.stringify(products));
+    alert(`Item removido.`);
+    products.sort((a, b) => a.price - b.price);
+    updateProductList()
 }
 
 // Botão para cadastrar novo produto
-document.getElementById('new-product-btn').addEventListener('click', function(){
+document.getElementById('new-product-btn').addEventListener('click', function () {
     window.location.href = 'index.html';
 });
 
